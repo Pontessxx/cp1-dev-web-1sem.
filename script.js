436665e00f56179184f32ade4d60f62a9650cd39
Henrique Pontes <126187491+Pontessxx@users.noreply.github.com>
2023-03-31 20:10:04 -0300

    2 Questão

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-/*Como declarar uma variável com valor undefined em JavaScript?  */
-let variavelUndefine;
+let n1 = 7;//leitura do valor X
+let n2 = 2;//leitura do valor y
+let div = n1 % n2;
+console.log(div);
