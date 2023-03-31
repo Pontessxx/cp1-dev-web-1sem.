9496aab0ee5b431e5a46755049460bc88b4705bf
Viniyama23 <126264900+Viniyama23@users.noreply.github.com>
2023-03-31 20:27:34 -0300

    3 - Questão: IMC

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let n1 = 7;//leitura do valor X
-let n2 = 2;//leitura do valor y
-let div = n1 % n2;
-console.log(div);
+/*Escreva  um  programa  que  calcula  o  índice  de  massa  corporal  (IMC)  
+de  uma  pessoa  e  exibe  uma mensagem indicando se ela está abaixo, no peso ideal ou acima do peso.
+ Considere as seguintes faixas de IMC:  abaixo  de  18,5  (abaixo  do  peso),
+   entre  18,5  e  24,9  (peso  ideal)  e  acima  de  24,9  (acima  do  peso).
+    Exemplifique e demonstre a saída! (10 Pontos) */
+
+let peso = prompt("Digite seu peso: ") 
+let altura = prompt("Digite sua altura: ")
+
+resultado = peso / altura**2
+
+if (resultado < 18.5){
+    document.write("você esta abaixo do peso")
+}
+else if (resultado > 18.5 && resultado <= 24.9){
+    document.write("você esta na media normal de peso")
+}
+else if (resultado > 24.9){
+    document.write("você esta acima do peso")
+}
