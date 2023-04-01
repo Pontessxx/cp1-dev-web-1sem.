6bcba11bc3f3dde517f3deb8f3627f55fa0ef31c
gukawamura <126724446+gukawamura@users.noreply.github.com>
2023-03-31 21:04:29 -0300

    exercício 8

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let notas = [];//array estrutura de dados que permite armazenar um conjunto de valores em uma única variável.
-let soma = 0;
-for (let i = 1; i <= 7; i++) { //media calculada
-  let nota = parseFloat(prompt(`Digite a nota ${i}:`));
-  notas.push(nota);
-  soma += nota;
-}
-let media = soma / 7;
-if (media >= 6) {
-  console.log(`Parabéns, você foi aprovado! Sua média foi ${media.toFixed(2)}.`);
-} else {
-  console.log(`Infelizmente você foi reprovado. Sua média foi ${media.toFixed(2)}.`);
-} 
+/*Crie um programa em Javascript que troque a palavra lugar por mundo da frase O LUGAR VIRA TECNOLOGIA e apresente na tela.*/
+
+
+let txt = ("O LUGAR VIRA TECNOLOGIA")
+console.log(txt.replace("LUGAR","MUNDO"))
+
+document.write(txt)
