608c9a8a30abb79f6649be8a1fbc4e5c1bba85c2
rcm2005 <68041167+rcm2005@users.noreply.github.com>
2023-03-31 21:17:32 -0300

    adicionando o exercicio 9

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ let txt = ("O LUGAR VIRA TECNOLOGIA")
 console.log(txt.replace("LUGAR","MUNDO"))
 
 document.write(txt)
+
+
+//exercicio 9 Crie um programa que declare a variável como String depois converta para float e em seguida apresente apenas dois números depois da virgula 
+
+
+let string = prompt('Digite a variável string: ')
+
+let float = parseFloat(string)
+
+console.log(float)
+
+let floatround = float.toFixed(2);
+
+console.log(floatround)
+document.write(floatround)
