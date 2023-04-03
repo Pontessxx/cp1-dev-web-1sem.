d285cdcb55ebb19a1d87f06ffcac1efcdfde8c80
Capriottiii <126483426+Capriottiii@users.noreply.github.com>
2023-04-03 08:09:46 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-/*Crie um programa em Javascript que troque a palavra lugar por mundo da frase O LUGAR VIRA TECNOLOGIA e apresente na tela.*/
+document.write("<br>")
+let senha = "1234"
+let admin = ["admin","Admin"];  // determinando quais nomes são classificados como adm
+var [primeiro, segundo] = admin  // parte do processo de designação da função admin
+//coloquei variantes com inicial minúscula para evitar erros
+let username = prompt('digite seu username: '); //solicita o nome do usuário
 
 
-let txt = ("O LUGAR VIRA TECNOLOGIA")
-console.log(txt.replace("LUGAR","MUNDO"))
-
-document.write(txt)
+let password = prompt('Digite a senha: ')
 
 
-//exercicio 9 Crie um programa que declare a variável como String depois converta para float e em seguida apresente apenas dois números depois da virgula 
-
-
-let string = prompt('Digite a variável string: ')
-
-let float = parseFloat(string)
-
-console.log(float)
-
-let floatround = float.toFixed(2);
-
-console.log(floatround)
-document.write(floatround)
+if(username == primeiro && password == senha){
+    document.write('usuário logado');  // testa a paridade entre o nome do imput e cada possível admin
+}                                       // ao mesmo tempo que averigua a veracidade da senha informada
+else if(username == segundo){
+    document.write('usuário logado');
+}
+else if(username != admin){
+    document.write('acesso negado');
+}
