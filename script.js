ae3a50b6a9c5636f375455147a0d78a9dbe13699
Capriottiii <126483426+Capriottiii@users.noreply.github.com>
2023-04-03 08:12:32 -0300

    exercicio 7

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
-document.write("<br>")
-let senha = "1234"
-let admin = ["admin","Admin"];  // determinando quais nomes são classificados como adm
-var [primeiro, segundo] = admin  // parte do processo de designação da função admin
-//coloquei variantes com inicial minúscula para evitar erros
-let username = prompt('digite seu username: '); //solicita o nome do usuário
+let nome = prompt("Digite o seu nome:");
+let idade = prompt("Digite a sua idade:");
+let curso = prompt("Digite o nome do seu curso:");
+let ano = prompt("Digite o ano do seu curso:");
 
-
-let password = prompt('Digite a senha: ')
-
-
-if(username == primeiro && password == senha){
-    document.write('usuário logado');  // testa a paridade entre o nome do imput e cada possível admin
-}                                       // ao mesmo tempo que averigua a veracidade da senha informada
-else if(username == segundo){
-    document.write('usuário logado');
-}
-else if(username != admin){
-    document.write('acesso negado');
-}
+document.write(`<p>Nome: ${nome}</p>`);
+document.write(`<p>Idade: ${idade}</p>`);
+document.write(`<p>Curso: ${curso}</p>`);
+document.write(`<p>Ano: ${ano}</p>`);
